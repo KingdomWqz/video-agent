7bb020a7b68053f4532d61e24c669932b4e54b7a
qinzheng_wang <qinzheng_wang@fzzixun.com>
2024-12-10 15:56:46 +0800

    feat: download youtube video

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+
+from download import download_youtube_video
+from upload import upload_file
 
 app = Flask(__name__)
 
@@ -6,6 +9,21 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
+
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/youtube/download', methods=['POST'])
+def download_video():
+    data = request.get_json()
+    url = data.get('url')
+
+    if not url:
+        return 'URL is required', 400
+
+    path = download_youtube_video(url)
+    url = upload_file('./' + path)
+
+    return url
---
+++ api/upload.py
@@ -0,0 +1,37 @@
+# from qiniu.services.storage.uploader import put_file
+# from qiniu.utils import etag
+# from qiniu.auth import Auth
+
+from qiniu import Auth, put_file, etag
+import os
+
+import qiniu.config
+
+def upload_file(localfile):
+    #构建鉴权对象
+    access_key = 'NHeMgdTNz7KsmVZJ4qH-gr-KGErKt6CFM-nON7JO'
+    secret_key = 'qxp0iiOU4cyIC1MWYXVkLgqDifxDIm1BwRADT4aa'
+    q = Auth(access_key, secret_key)
+
+    #要上传的空间
+    bucket_name = 'showhype-test'
+    
+    #上传后保存的文件名
+    key = 'test.mp4'
+    #生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
+    #要上传文件的本地路径
+    ret, info = put_file(token, key, localfile, version='v2')
+    
+    print(info)
+    assert ret['key'] == key
+    assert ret['hash'] == etag(localfile)
+    
+    # 删除本地文件
+    if os.path.exists(localfile):
+        os.remove(localfile)
+        print(f"{localfile} has been deleted.")
+    else:
+        print(f"{localfile} does not exist.")
+    
+    return 'http://so7r90nt8.hn-bkt.clouddn.com/' + key
